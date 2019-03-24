2f20edced7ebb37391e75e641dbabaa07d44b8ae
blessedsibanda <n01519451g@students.nust.ac.zw>
2019-03-24 11:21:15 +0200

    update views

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Note
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    notes = Note.objects.all()
+    return render(request, 'home.html', {'notes': notes})
