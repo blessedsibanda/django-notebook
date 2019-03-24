967919af105c57fc51cb749df0363c40f6c8a1c5
blessedsibanda <n01519451g@students.nust.ac.zw>
2019-03-24 11:26:14 +0200

    create note detail view

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('<id>/', views.note_detail, name='note_detail'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Note
 
@@ -6,3 +6,7 @@ from .models import Note
 def home(request):
     notes = Note.objects.all()
     return render(request, 'home.html', {'notes': notes})
+
+def note_detail(request, id):
+    note = get_object_or_404(Note,id=id)
+    return render(request, 'detail.html', {'note': note})
