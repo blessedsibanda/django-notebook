d123bae281604ac6f057b60dc26d6c4167429e70
blessedsibanda <n01519451g@students.nust.ac.zw>
2019-03-24 14:00:38 +0200

    delete note

--- notes/urls.py
+++ notes/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('note/<int:id>/', views.note_detail, name='note_detail'),
     path('note/add/', views.add_note, name='add_note'),
+    path('note/<int:id>/edit/', views.edit_note, name='edit_note'),
+    path('note/<int:id>/delete/', views.delete_note, name='delete_note'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -22,3 +22,21 @@ def add_note(request):
             return redirect(reverse('note_detail', kwargs={'id':new_note.id}))
         return render(request, 'add_note.html', {'form':form})
     return render(request, 'add_note.html', {'form':form})
+
+def edit_note(request, id):
+    note = get_object_or_404(Note, id=id)
+    form = NoteForm(instance=note)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            updated_note = form.save()
+            return redirect(reverse('note_detail', kwargs={'id':updated_note.id}))
+        return render(request, 'edit_note.html', {'form':form})
+    return render(request, 'edit_note.html', {'form':form})
+
+def delete_note(request, id):
+    note = get_object_or_404(Note, id=id)
+    if request.method == 'POST':
+        note.delete()
+        return redirect(reverse('home'))
+    return render(request, 'delete_note.html', {'note':note})
